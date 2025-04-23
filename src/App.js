84616e53ede8789e71a7da998553f70e63474072
mikwieleb <mik.wieleb@gmail.com>
2025-04-24 01:35:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ function App() {
     <div className="App">
       <img src="/favicon.ico" alt="Logo Pi" width="100" />
       <h1>Bienvenue sur l'app Pi Test</h1>
-      <button onClick={() => window.open('https://app.pi', '_blank')}>
-        Ouvrir l'application
-      </button>
-      <br /><br />
       <PiPaymentButton />
     </div>
   );
