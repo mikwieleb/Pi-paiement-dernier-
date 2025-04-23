531b1322ef2d73f078747a8d8442918041d71e70
mikwieleb <mik.wieleb@gmail.com>
2025-04-24 01:40:30 +0200

    Update verify-payment.js

--- pages/api/verify-payment.js
+++ pages/api/verify-payment.js
@@ -16,6 +16,6 @@ export default async function handler(req, res) {
     return res.status(200).json({ success: true });
   } catch (error) {
     console.error('Erreur durant le paiement:', error);
-    res.status(500).json({ error: 'Erreur serveur' });
+    return res.status(500).json({ error: 'Erreur serveur' });
   }
 }
